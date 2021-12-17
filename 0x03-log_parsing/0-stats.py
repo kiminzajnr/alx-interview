395ec01b8a21e3f86f489206654ccdf376127501
Erick Kiminza <kiminzaeric@gmail.com>
2021-12-17 11:59:35 +0300

    first commit

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Log parsing
+"""
+import fileinput
+from typing import List
+
+
+line_count = 0
+total_size = 0
+status_codes = []
+
+for line in fileinput.input():
+    ln = line.split()
+
+    if len(ln) != 9:
+        continue
+
+    line_count += 1
+    status_codes.append(ln[7])
+    total_size += int(ln[8])
+
+    if line_count == 10:
+        for status in set(sorted(status_codes)):
+            counts = status_codes.count(status)
+            print("{}: {}".format(status, counts))
+
+        print("File size: {}".format(total_size))
+        line_count = 0
+        total_size = 0

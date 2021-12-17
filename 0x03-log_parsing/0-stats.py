74020e1b24ee1126c2c2e08084e386e25004fc13
Erick Kiminza <kiminzaeric@gmail.com>
2021-12-17 14:50:12 +0300

    fix output error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Log parsing: reads a stdin line by line and computes metrics
+Log parsing reads a stdin line by line and computes metrics
 """
 import fileinput
 from typing import List
@@ -20,11 +20,20 @@ for line in fileinput.input():
     status_codes.append(ln[7])
     total_size += int(ln[8])
 
-    if line_count == 10:
+    try:
+        if line_count == 10:
+            print("File size: {}".format(total_size))
+            for status in set(sorted(status_codes)):
+                counts = status_codes.count(status)
+                print("{}: {}".format(status, counts))
+
+            line_count = 0
+            total_size = 0
+    except KeyboardInterrupt:
         for status in set(sorted(status_codes)):
             counts = status_codes.count(status)
             print("{}: {}".format(status, counts))
-
         print("File size: {}".format(total_size))
         line_count = 0
         total_size = 0
+        raise
